8de5cc6d05ef99b2ee6bb5b2efff1d431d101ba9
yazan <yazeno15@gmail.com>
2020-06-29 21:17:55 +0300

    system with Auth
    adding Authentication for account companies.

--- database/migrations/2020_06_29_175942_users_migration.php
+++ database/migrations/2020_06_29_175942_users_migration.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class UsersMigration extends Migration
 {
     /**
      * Run the migrations.
@@ -32,6 +32,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 }
